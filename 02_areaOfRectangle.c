5e085b59aee6a46061ba350bc53682895a8f36e0
Homanath Adhikari <160689013+homanath@users.noreply.github.com>
2024-08-13 21:27:17 +0545

    Add files via upload

---
+++ 02_areaOfRectangle.c
@@ -0,0 +1,12 @@
+//program to clacualte area of arectangle
+#include<stdio.h>
+
+int main(){
+    int length,breadth;
+    printf("Enter length and breadth\n");
+    scanf("%d%d",&length,&breadth);
+     
+    printf("The area of a rectangle is %d",length*breadth);
+
+    return 0;
+}
