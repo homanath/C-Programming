9f043d93dfb31329646f603e466245b5e4e6bedb
Homanath Adhikari <160689013+homanath@users.noreply.github.com>
2024-08-14 21:54:50 +0545

    Add files via upload

---
+++ 03_areaOfCircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int r=6;       //r=radius
+    int h=10;      //h=height
+    printf("The area of circle of redius %d is %f \n" ,r, 3.14*r*r);
+    printf("The volume of circle of redius %d and height %d is %f" ,r,h, 3.14*r*r*h);
+    return 0;
+}
